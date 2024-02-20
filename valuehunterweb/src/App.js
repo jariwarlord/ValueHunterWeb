896db8f86505ae4156a40b0500ee14f67d4fb476
jariwarlord <berkedalar4@gmail.com>
2024-02-20 23:28:51 +0300

    navbar added with little bugs

--- valuehunterweb/src/App.js
+++ valuehunterweb/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import PasswordResetForm from './PasswordResetForm'; // Import the password reset form component
+import Navbar from './components/Navbar';
 
 function App() {
   return (
+    
     <div className="App">
+      <Navbar/>
       <h1 style={{width:"80px"}}>Password Reset Form</h1>
       <PasswordResetForm /> {/* Render the password reset form */}
     </div>
---
+++ valuehunterweb/src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles/Navbar.css'; // Stil dosyası
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      <p>Password Reset</p>
+
+    </div>
+  );
+}
+
+export default Navbar;
